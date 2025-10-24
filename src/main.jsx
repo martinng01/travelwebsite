03f8637dc97861fa79c385c3128630296c5664c8
Martin <martin.ng@live.com>
2025-10-24 18:40:38 +0800

    add mantine

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import GlobeComponent from "./Globe.jsx";
+import { MantineProvider } from "@mantine/core";
 import "./index.css";
+import "@mantine/core/styles.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GlobeComponent />
+    <MantineProvider>
+      <GlobeComponent />
+    </MantineProvider>
   </StrictMode>
 );
