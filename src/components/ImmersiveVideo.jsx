d73a366a8cd60f7c29812bdc5ea71a9161112755
Martin <martin.ng@live.com>
2025-10-28 21:52:34 +0800

    add video

---
+++ src/components/ImmersiveVideo.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Center, Container } from "@mantine/core";
+
+function ImmersiveVideo({ src }) {
+  return (
+    <Container px={0} py={0} mx="auto" size="60%" style={{ height: "100vh" }}>
+      <video
+        autoPlay
+        muted
+        loop
+        style={{
+          height: "100%",
+          width: "100%",
+          objectFit: "cover",
+          display: "block",
+        }}
+      >
+        <source src={src} type="video/mp4" />
+      </video>
+    </Container>
+  );
+}
+
+export default ImmersiveVideo;
